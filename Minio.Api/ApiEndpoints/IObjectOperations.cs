1431d39aa684afd6b0b7064f16bb6eb8d74f4d72
poornas <k.poorna@gmail.com>
2017-01-20 17:14:33 -0800

    implemented statobject call

--- Minio.Api/ApiEndpoints/IObjectOperations.cs
+++ Minio.Api/ApiEndpoints/IObjectOperations.cs
@@ -11,16 +11,18 @@ namespace Minio
     public interface IObjectOperations
     {
         Task GetObjectAsync(string bucketName, string objectName, Action<Stream> callback);
+        //Task PutObjectAsync(string bucketName, string objectName, Stream data, long size, string contentType);
+        Task RemoveObjectAsync(string bucketName, string objectName);
+        Task<ObjectStat> StatObjectAsync(string bucketName, string objectName);
+
         /*
          * To be implemented
      
         //accepts file instead of stream
         Task GetObjectAsync(string bucketName, string objectName,string filePath);
 
-        Task PutObjectAsync(string bucketName, string objectName, Stream data, long size, string contentType,string fileName="");
         //accepts file instead of stream
         Task PutObjectAsync(string bucketName, string objectName, string filePath, string contentType);
-        Task<ObjectStat> StatObjectAsync(string bucketName, string objectName);
         Task CopyObjectAsync(string bucketName, string objectName, string objectSource, CopyConditions conditions);
         Task RemoveObjectAsync(string bucketName, string objectName);
         //task RemoveObjects(string bucketName, Stream objectsList );
--- Minio.Api/ApiEndpoints/ObjectOperations.cs
+++ Minio.Api/ApiEndpoints/ObjectOperations.cs
@@ -47,10 +47,62 @@ namespace Minio
          
             return;
         }
-     
 
-      
-    private static string UrlEncode(string input)
+        public async Task PutObjectAsync(string bucketName, string objectName, Stream data, long size, string contentType)
+        {
+
+        }
+        public async Task RemoveObjectAsync(string bucketName, string objectName)
+        {
+            var request = new RestRequest(bucketName + "/" + UrlEncode(objectName), Method.DELETE);
+            var response = await this._client.ExecuteTaskAsync(this._client.NoErrorHandlers, request);
+
+
+            if (!response.StatusCode.Equals(HttpStatusCode.NoContent))
+            {
+                throw this._client.ParseError(response);
+            }
+        }
+
+        public async Task<ObjectStat> StatObjectAsync(string bucketName, string objectName)
+        {
+            var request = new RestRequest(bucketName + "/" + UrlEncode(objectName), Method.HEAD);
+            var response = await this._client.ExecuteTaskAsync(this._client.NoErrorHandlers, request);
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw this._client.ParseError(response);
+            }
+         
+            long size = 0;
+            DateTime lastModified = new DateTime();
+            string etag = "";
+            string contentType = null;
+            foreach (Parameter parameter in response.Headers)
+            {
+                switch (parameter.Name)
+                {
+                    case "Content-Length":
+                        size = long.Parse(parameter.Value.ToString());
+                        break;
+                    case "Last-Modified":
+                        lastModified = DateTime.Parse(parameter.Value.ToString());
+                        break;
+                    case "ETag":
+                        etag = parameter.Value.ToString().Replace("\"", "");
+                        break;
+                    case "Content-Type":
+                        contentType = parameter.Value.ToString();
+                        break;
+                    default:
+                        break;
+                }
+            }
+            return new ObjectStat(objectName, size, lastModified, etag, contentType);
+           
+        }
+
+        private static string UrlEncode(string input)
         {
             return Uri.EscapeDataString(input).Replace("%2F", "/");
         }
